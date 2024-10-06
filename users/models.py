08b8078701d2632c47fbb3ad035705d35cb6b45c
realman2k <53941338+realman2k@users.noreply.github.com>
2024-10-06 23:47:54 +0300

    Добавлена модель пользователя.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, **NULLABLE)
+    address = models.TextField(**NULLABLE)
+    date_of_birth = models.DateField(**NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
