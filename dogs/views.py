6d2c4df5d43e298dae59d174bd4b849fd67c3407
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-10-16 23:58:31 +0300

    Написаны FBV: index categories category_dogs А также пути для них

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from dogs.models import Dog, Category
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'object_list': Category.objects.all()[:3],
+        'title': 'Питомник - Главная'
+    }
+
+    return render(request, 'dogs/index.html', context)
+
+
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Питомник - Все наши породы'
+    }
+
+    return render(request, 'dogs/categories.html', context)
+
+def category_dogs(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Dog.objects.filter(category_id=pk),
+        'title': 'Питомник - собаки породы ' + category_item.name,
+        'category_pk': category_item.pk,
+    }
+
+    return render(request, 'dogs/dogs.html', context)
