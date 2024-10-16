c42a401b8a3e3ed79108ccb0800d8e2fbd726ec6
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-10-16 21:51:38 +0300

    Добавлено приложение dogs в настройки, успешно добавлена модель Category, миграции проведены.

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="breed")
+    description = models.CharField(max_length=1000, verbose_name="description")
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'breed'
+        verbose_name_plural = 'breeds'
+
