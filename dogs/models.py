9bf216544b7c1943e89823fe5ccfc4cdd0e51b11
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-10-16 22:08:56 +0300

    Добавлена модель Dogs, миграции проведены.

--- dogs/models.py
+++ dogs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from users.models import NULLABLE
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="breed")
     description = models.CharField(max_length=1000, verbose_name="description")
@@ -13,3 +14,15 @@ class Category(models.Model):
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
 
+class Dogs(models.Model):
+    name = models.CharField(max_length=250, verbose_name="dog_name")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="breed")
+    photo = models.ImageField(upload_to='dogs/', verbose_name="photo", **NULLABLE)
+    birth_date = models.DateField(verbose_name="birth_date", **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
