8a83ed3c336ee01f388e2778a154b538c1ac2a58
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-11-30 23:50:12 +0300

    Доработана команда на создание БД

--- users/management/commands/create_db.py
+++ users/management/commands/create_db.py
@@ -1,10 +1,34 @@
 from django.core.management.base import BaseCommand
-from django.db import connection
+import pyodbc
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Command(BaseCommand):
     help = 'Create the database'
 
-    def handle(self, *args, **kwargs):
-        with connection.cursor() as cursor:
-            cursor.execute("CREATE DATABASE DjangoProject;")
-        self.stdout.write(self.style.SUCCESS('Database created successfully'))
+    def handle(self, *args, **options):
+        ODBC_DRIVER = os.getenv('ODBC_DRIVER')
+        SERVER = os.getenv('SERVER')
+        USER = os.getenv('DUSER')
+        PASSWORD = os.getenv('PASSWORD')
+        PAD_DATABASE = os.getenv('PAD_DATABASE')
+        DATABASE = 'DjangoCBVProjectDB'
+
+        ConnectionString = f'''DRIVER={{{ODBC_DRIVER}}};
+        SERVER={SERVER};
+        DATABASE={PAD_DATABASE};
+        UID={USER};
+        PWD={PASSWORD};
+        TrustServerCertificate=yes;
+        Encrypt=no'''
+
+        try:
+            conn = pyodbc.connect(ConnectionString)
+            conn.autocommit = True
+            conn.execute(fr"CREATE DATABASE {DATABASE};")
+        except pyodbc.ProgrammingError as ex:
+            print(ex)
+        else:
+            print("База данных успешно создана")
