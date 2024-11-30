cfc2887837d9c717b63f5994bb4e4f54f2a173ee
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-11-30 14:05:08 +0300

    Исправлены ошибки в валидаторе

--- users/validators.py
+++ users/validators.py
@@ -1,11 +1,9 @@
 import re
 from django.core.exceptions import ValidationError
 
-def validate_password (field):
+def validate_password(field):
     pattern = re.compile(r'^[A-Za-z0-9]+$')
-    if not bool(re.match(pattern, field)):
-     # print( 'Must contain A-Z, a-z letters and 0-9 numbers')
+    if not pattern.match(field):
         raise ValidationError("Must contain A-Z, a-z letters and 0-9 numbers")
-    if not 6 <= len (field) <= 12:
-    # print('Password length must be between 6 and 12 charters')
-        raise ValidationError("Password length must be between 6 and 12 charters")
+    if not 8 <= len(field) <= 12:
+        raise ValidationError("Password length must be between 8 and 12 characters")
