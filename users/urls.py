307af83fb2036dfdd6aa0abaf94a0805d5a38198
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-01 01:37:52 +0300

    fbv user_update_view переписано на cbv UserUpdateView.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import  user_logout_view, user_udate_view, \
-    user_change_password_view,  user_generate_new_password, UserRegisterView, UserLoginView, UserProfileView
+from users.views import user_logout_view, user_change_password_view, user_generate_new_password, UserRegisterView, \
+    UserLoginView, UserProfileView, UserUpdateView
 
 app_name = UsersConfig.name
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', user_logout_view, name='logout_user'),
     path('register/', UserRegisterView.as_view(), name='register_user'),
     path('profile/', UserProfileView.as_view(), name='profile_user'),
-    path('update/', user_udate_view, name='update_user'),
+    path('update/', UserUpdateView.as_view(), name='update_user'),
     path('change_password/', user_change_password_view, name='change_password_user'),
     path('profile/genpassword/', user_generate_new_password, name='user_generate_new_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -31,29 +31,21 @@ class UserLoginView(LoginView):
 
 class UserProfileView(UpdateView):
     model = User
-    form_class = UserForm
+    form_class = UserUpdateForm
     template_name = 'users/user_profile_read_only.html'
 
     def get_object(self, queryset=None):
         return self.request.user
 
 
-@login_required
-def user_udate_view(request):
-    user_object = request.user
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, request.FILES, instance=user_object)
-        if form.is_valid():
-            user_object = form.save()
-            user_object.save()
-            return redirect('users:profile_user')
-    user_name = user_object.first_name
-    context = {
-        'user_object': user_object,
-        'title': f' Изменить профиль пользователя {user_name}',
-        'form': UserUpdateForm(instance=user_object),
-    }
-    return render(request, 'users/update_user.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'users/update_user.html'
+    success_url = reverse_lazy('users:profile_user')
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 @login_required
