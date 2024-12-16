141af2d9a3ceb597a36c5c6454b9557c8b1b217a
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-16 14:57:39 +0700

    Добавлена функция отправки письма

--- dogs/templates/dogs/services.py
+++ dogs/templates/dogs/services.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.core.cache import cache
+from django.core.mail import send_mail
 from dogs.models import Category
-
+from dogs.models import Dog
 
 def get_categories_cache():
     if settings.CACHE_ENABLED:
@@ -14,3 +15,11 @@ def get_categories_cache():
         category_list = Category.objects.all()
 
     return category_list
+
+
+def send_email(dog_object, owner_email, views_count):
+    subject = f'Ваша собака "{dog_object}" была просмотрена {views_count} раз'
+    message = f'Поздравляем! Ваша собака "{dog_object}" была просмотрена {views_count} раз'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [owner_email,]
+    send_mail(subject, message, email_from, recipient_list)
