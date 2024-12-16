188d55c8acd7ae51a8f437db4a95aa109b0c7aa0
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-16 14:55:06 +0700

    Обновлена модель добавлены миграции

--- dogs/models.py
+++ dogs/models.py
@@ -41,6 +41,7 @@ class Dog(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.SET_NULL, **NULLABLE,
                               verbose_name="Владелец")
     is_active = models.BooleanField(default=True, verbose_name="is_active")
+    views = models.PositiveIntegerField(default=0, verbose_name="просмотры")
 
     def __str__(self):
         return f'{self.name} ({self.category})'
@@ -48,6 +49,10 @@ class Dog(models.Model):
     class Meta:
         verbose_name = 'dog'
         verbose_name_plural = 'dogs'
+
+    def views_count(self):
+        self.views += 1
+        self.save()
 
 class Parent(models.Model):
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
