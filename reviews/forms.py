c44327f3bf635db3349f28ede7a38f66f6fec608
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-17 16:31:24 +0700

    Добавлена модель отзыва, зарегистрирована в админке, написана форма для нее.

---
+++ reviews/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from reviews.models import Review
+from dogs.forms import StyleFormMixin
+
+class ReviewForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('dog', 'title', 'content', 'slug')
+        include = ['title', 'content', 'slug', ]
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from users.models import NULLABLE
 
-# Create your models here.
+from dogs.models import Dog
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок', **NULLABLE)
+    slug = models.SlugField(max_length=25, unique=True, db_index=True, verbose_name='URL')
+    content = models.TextField(verbose_name='Содержимое', **NULLABLE)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    sign_of_review = models.BooleanField(default=True, verbose_name='Активный отзыв')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Автор')
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE, verbose_name='Собака')
+
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse('reviews:detail_review', kwargs={'slug': self.slug})
+
