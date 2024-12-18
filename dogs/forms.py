56685d0a4a764d4ebe7c5c685788462395f503a0
Evgeniy Koshlakov <evgeniy@koshlakov.com>
2024-12-18 15:04:10 +0700

    Исправлена форма

--- dogs/forms.py
+++ dogs/forms.py
@@ -29,9 +29,18 @@ class DogAdminForm(StyleFormMixin, forms.ModelForm):
         model = Dog
         fields = '__all__'
 
-    @staticmethod
-    def clean_birth_date():
-        DogForm.clean_birth_date()
+    def clean_birth_date(self):
+        if self.cleaned_data['birth_date']:
+            cleaned_data = self.cleaned_data['birth_date']
+            now_year = datetime.datetime.now().year
+            if cleaned_data.year > now_year:
+                raise forms.ValidationError('Дата рождения не может быть в будущем')
+            if cleaned_data.year < now_year - 30:
+                raise forms.ValidationError('Покойников не берем.')
+
+            return cleaned_data
+        else:
+            return self.cleaned_data['birth_date']
 
 
 class ParentForm(StyleFormMixin, forms.ModelForm):
